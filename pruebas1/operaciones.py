24b9f21d8a89bbac705bdf2b7d15a5b4022f46e9
maveloc <luisrguezprimera@gmail.com>
2025-08-31 14:48:07 +0200

    operandos

---
+++ pruebas1/operaciones.py
@@ -0,0 +1,23 @@
+#OPERACIONES
+# +	Addition	x + y	
+# -	Subtraction	x - y	
+# *	Multiplication	x * y	
+# /	Division	x / y	
+# %	Modulus	x % y	
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y
+
+# Operator	Example	     Same As
+# =	x = 5	x = 5	
+# +=	    x += 3	    x = x + 3	
+# -=	    x -= 3	    x = x - 3	
+# *=	    x *= 3	    x = x * 3	
+# /=	    x /= 3	    x = x / 3	
+# %=	    x %= 3	    x = x % 3	
+# //=	    x //= 3	    x = x // 3	
+# **=	    x **= 3	    x = x ** 3	
+# &=	    x &= 3	    x = x & 3	
+# |=	    x |= 3	    x = x | 3	
+# ^=	    x ^= 3	    x = x ^ 3	
+# >>=	    x >>= 3	    x = x >> 3	
+# <<=	    x <<= 3	    x = x << 3	
