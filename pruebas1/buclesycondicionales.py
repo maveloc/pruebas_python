afc20eb965ce0b58a3a2f015b8e98455aa77a8c4
maveloc <luisrguezprimera@gmail.com>
2025-09-04 14:55:42 +0200

    falta for loops

---
+++ pruebas1/buclesycondicionales.py
@@ -0,0 +1,29 @@
+x= 50
+y=20 
+#if else, mitico, no hay fallo
+if x<y:
+    print("a")
+else: 
+    print ("b")
+
+#match case es lo mismo que switch case
+dia = 3
+
+match dia:
+    case 1 | 2 | 4 | 5 if x == 50:
+        print ("sabado")
+    case 3:
+        print("lunes")
+    case _:
+        print("Julio")
+
+#while loops
+i=0
+while i < 10:
+    i+=1
+    print(i)
+    if i == 10:
+        continue #con continue rompemos el bucle, con un else al final puedes hacer algo
+    #cuando se ha cumplico la condicion del while
+
+
